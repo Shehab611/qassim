03f45fe19e4a90e3bc2bbe556119d24eed0f2e39
sheha <shehabehab@gmail.com>
2023-12-08 17:34:42 +0200

    edit main file for localization

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
 abstract final class AppConstants{
   static List locale = ['en', 'ar',];
   static String defaultLanguage = 'ar';
+  static List<Locale> supportedLocales = const [
+    Locale('en', 'US'),
+    Locale('ar', ''),
+  ];
 }
