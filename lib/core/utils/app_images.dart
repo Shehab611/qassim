5f794635073035d0319707b3203041be72a26355
sheha <shehabehab@gmail.com>
2023-12-09 10:07:43 +0200

    add images file

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,7 @@
+abstract final class AppImages{
+  static const String blackDarkLogo = 'assets/images/black_dark_logo.png';
+  static const String blackLightLogo = 'assets/images/black_light_logo.png';
+  static const String blackLightLogoo = 'assets/images/black_light_logoo.png';
+  static const String whiteDarkLogo = 'assets/images/white_dark_logo.png';
+  static const String whiteLogo = 'assets/images/white_logo.png';
+}
