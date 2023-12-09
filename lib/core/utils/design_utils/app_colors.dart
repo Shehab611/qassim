7b02854aded4561ca378211a553d71f20d47bb4d
sheha <shehabehab@gmail.com>
2023-12-09 13:41:36 +0200

    add new colors to colors file

--- lib/core/utils/design_utils/app_colors.dart
+++ lib/core/utils/design_utils/app_colors.dart
@@ -2,6 +2,26 @@ import 'package:flutter/material.dart';
 
 abstract final class AppColors {
   static const primaryColor = Color(0xffEDDDAB);
-  static const elevatedButtonBackground = Color(0xff325554);
+  static const elevatedButtonBackground = complementaryColor1;
   static const elevatedButtonTextColor = Color(0xffFFFAE0);
+  static const darkerShadeColor1  = Color(0xffC6BB93);
+  static const darkerShadeColor2  = Color(0xffA89F76);
+  static const darkerShadeColor3  = Color(0xff8B835B);
+  static const darkerShadeColor4  = Color(0xff726B45);
+  static const darkerShadeColor5  = Color(0xff56503A);
+  static const darkerShadeColor6  = Color(0xff3B3526);
+
+  static const lighterShadeColor1  = Color(0xffFFFAE0);
+  static const lighterShadeColor2  = Color(0xffFFFCF2);
+  static const lighterShadeColor3  = Color(0xffFFFDF7);
+  static const lighterShadeColor4  = Color(0xffFFFDE6);
+  static const lighterShadeColor5  = Color(0xffFFFEE9);
+  static const lighterShadeColor6  = Color(0xffFFFFEC);
+
+
+
+  static const complementaryColor1 = Color(0xff325554);
+  static const complementaryColor2 = Color(0xff324455);
+  static const complementaryColor3 = Color(0xff553232);
+  static const complementaryColor4 = Color(0xff453232);
 }
