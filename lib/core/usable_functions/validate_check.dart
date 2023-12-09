5733ec5216aa93e10b5eb36cba2bcb9e0a396e89
sheha <shehabehab@gmail.com>
2023-12-09 12:27:11 +0200

    edit validate check file and localizations json files

--- lib/core/usable_functions/validate_check.dart
+++ lib/core/usable_functions/validate_check.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:qassim/core/utils/app_localization.dart';
 
-class ValidateCheck{
-  static String? validateEmail(String? value,BuildContext context,) {
+class ValidateCheck {
+  static String? validateEmail(
+    String? value,
+    BuildContext context,
+  ) {
     const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
         r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
         r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
@@ -13,34 +16,49 @@ class ValidateCheck{
     final kEmailValid = RegExp(pattern);
     bool isValid = kEmailValid.hasMatch(value.toString());
     if (value!.isEmpty) {
-      return '\u26A0 ${AppLocalizations.of(context).translate('EMAIL_MUST_BE_REQUIRED')}';
+      return '\u26A0 ${AppLocalizations.of(context).translate('email_is_required')}';
     } else if (isValid == false) {
-      return '\u26A0 ${AppLocalizations.of(context).translate("enter_valid_email_address",)}';
+      return '\u26A0 ${AppLocalizations.of(context).translate(
+        "invalid_email_address",
+      )}';
     }
     return null;
   }
 
-  static String? validateEmptyText(String? value,BuildContext context, String? message) {
+  static String? validateEmptyText(
+      String? value, BuildContext context, String? message) {
     if (value == null || value.isEmpty) {
       return AppLocalizations.of(context).translate(message!);
     }
     return null;
   }
 
-  static String? validatePassword(String? value,BuildContext context, String? message) {
+  static String? validatePassword(
+      String? value, BuildContext context,) {
     if (value == null || value.isEmpty) {
-      return AppLocalizations.of(context).translate(message!,);
-    }else if(value.length < 7){
-      return AppLocalizations.of(context).translate(AppLocalizations.of(context).translate('minimum_password_is_8_character', ), );
+      return AppLocalizations.of(context).translate(
+        'password_is_required',
+      );
+    } else if (value.length < 7) {
+      return AppLocalizations.of(context).translate(
+        AppLocalizations.of(context).translate(
+          'password_chars_is_not_valid',
+        ),
+      );
     }
     return null;
   }
 
-  static String? validateConfirmPassword(String? value,BuildContext context, String? password) {
+  static String? validateConfirmPassword(
+      String? value, BuildContext context, String? password) {
     if (value == null || value.isEmpty) {
-      return AppLocalizations.of(context).translate('confirm_password_must_be_required', );
-    }else if(value != password){
-      return AppLocalizations.of(context).translate('confirm_password_not_matched', );
+      return AppLocalizations.of(context).translate(
+        'confirm_password_required',
+      );
+    } else if (value != password) {
+      return AppLocalizations.of(context).translate(
+        'confirm_password_not_matched',
+      );
     }
     return null;
   }
