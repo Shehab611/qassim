88a7fff2da941587dcff4219a07ddffb2c0b0497
sheha <shehabehab@gmail.com>
2023-12-09 13:52:45 +0200

    make design structure of login design

--- lib/features/authentication/presentation/views/login_screen.dart
+++ lib/features/authentication/presentation/views/login_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:qassim/core/components/custom_text_field.dart';
+import 'package:qassim/core/utils/app_localization.dart';
 import 'package:qassim/core/utils/design_utils/app_images.dart';
+import 'package:qassim/core/utils/design_utils/app_sizes.dart';
+import 'package:qassim/core/utils/design_utils/app_text_styles.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,11 +11,35 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        Center(
-          child: Image.asset(AppImages.blackLightLogo),
-        ),
-      ]),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Image.asset(AppImages.blackLightLogo,),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.465,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: AppSizes.paddingSizeDefault),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CustomTextField(
+                    labelText:AppLocalizations.of(context).translate('email'),
+
+                  ),
+                  CustomTextField(
+                    labelText:AppLocalizations.of(context).translate('password'),
+                    isPassword: true,
+                  ),
+                  TextButton(onPressed: (){}, child: Text('rgloijigor')),
+                  ElevatedButton(onPressed: (){}, child: Text('fffjh',style: AppTextStyles.elevatedButtonTextStyle,)),
+                ],
+              ),
+            ),
+          ),
+          TextButton(onPressed: (){}, child: Text('rgloijigor')),
+
+        ]),
+      ),
     );
   }
 }
