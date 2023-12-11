160b3872f8fa4dde8fcbdfef383a4ceedff3c299
sheha <shehabehab@gmail.com>
2023-12-11 19:38:58 +0200

    finish login screen

--- lib/features/authentication/presentation/views/login_screen.dart
+++ lib/features/authentication/presentation/views/login_screen.dart
@@ -14,7 +14,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+     // backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child:
             Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
