1e2c0d03373e2a7d29cc285bad025fd9c8e1771f
sheha <shehabehab@gmail.com>
2023-12-13 00:50:21 +0200

    add otp verification widget

---
+++ lib/features/authentication/presentation/widgets/otp_verification_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class OtpVerificationWidget extends StatefulWidget {
+  const OtpVerificationWidget({super.key});
+
+  @override
+  State<OtpVerificationWidget> createState() => _OtpVerificationWidgetState();
+}
+
+class _OtpVerificationWidgetState extends State<OtpVerificationWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
