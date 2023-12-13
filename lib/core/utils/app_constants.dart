3831a6a4ed6490ba3946f3cde596c5e3589b485a
sheha <shehabehab@gmail.com>
2023-12-14 00:40:10 +0200

    add some api handlers

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 abstract final class AppConstants {
 
+  //#region private variables
   static const List<String> _locale = [
     'en',
     'ar',
@@ -11,10 +12,27 @@ abstract final class AppConstants {
     Locale('en', 'US'),
     Locale('ar', ''),
   ];
+  static const Duration _connectionTimeOut = Duration(seconds: 30000);
+  //#endregion
 
+  //#region getters
   static List<String> get locale => _locale;
 
   static String get defaultLanguage => _defaultLanguage;
 
   static List<Locale> get supportedLocales => _supportedLocales;
+
+  static Duration get connectionTimeOut => _connectionTimeOut;
+  //#endregion
+
+
+  //#region private shared preferences keys
+  static const String _userLoginToken = 'userLoginToken';
+
+  //#endregion
+
+
+  //#region shared preferences keys getters
+  static String get userLoginToken=>_userLoginToken;
+//#endregion
 }
