f2fdd939e35d6b5885f882ae7b807c7747465a73
sheha <shehabehab@gmail.com>
2023-12-14 04:49:47 +0200

    wrap the register screen with its cubit provider in routes

--- lib/core/utils/app_routes_utils/app_router.dart
+++ lib/core/utils/app_routes_utils/app_router.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:qassim/features/authentication/presentation/views/forget_password_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qassim/core/usable_functions/api_service_helper.dart';
+import 'package:qassim/features/authentication/data/repositories/register/register_repo_impl.dart';
+import 'package:qassim/features/authentication/presentation/view_model_manger/register_cubit/register_cubit.dart';
 import 'package:qassim/features/authentication/presentation/views/change_password_screen.dart';
+import 'package:qassim/features/authentication/presentation/views/forget_password_screen.dart';
 import 'package:qassim/features/authentication/presentation/views/login_screen.dart';
 import 'package:qassim/features/authentication/presentation/views/open_screen.dart';
 import 'package:qassim/features/authentication/presentation/views/otp_screen.dart';
 import 'package:qassim/features/authentication/presentation/views/register_screen.dart';
+import 'package:qassim/service_locator.dart';
 
 import 'app_paths.dart';
 
@@ -12,9 +17,14 @@ abstract final class AppRouter {
   static final Map<String, WidgetBuilder> routes = {
     AppPathName.kOpenScreen: (BuildContext context) => const OpenScreen(),
     AppPathName.kLoginScreen: (BuildContext context) => const LoginScreen(),
-    AppPathName.kRegisterScreen: (BuildContext context) => const RegisterScreen(),
+    AppPathName.kRegisterScreen: (BuildContext context) => BlocProvider(
+          create: (context) => RegisterCubit(RegisterRepoImpl(sl<DioClient>())),
+          child: const RegisterScreen(),
+        ),
     AppPathName.kOtpScreen: (BuildContext context) => const OtpScreen(),
-    AppPathName.kForgetPasswordScreen: (BuildContext context) => const ForgetPasswordScreen(),
-    AppPathName.kChangePasswordScreen: (BuildContext context) => const ChangePasswordScreen(),
+    AppPathName.kForgetPasswordScreen: (BuildContext context) =>
+        const ForgetPasswordScreen(),
+    AppPathName.kChangePasswordScreen: (BuildContext context) =>
+        const ChangePasswordScreen(),
   };
 }
