069cc9357d88b3f3a7b6a4f33d5ced901a96c315
sheha <shehabehab@gmail.com>
2023-12-14 04:34:24 +0200

    wrap the register screen with its cubit

--- lib/features/authentication/presentation/views/register_screen.dart
+++ lib/features/authentication/presentation/views/register_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qassim/core/components/custom_text_field.dart';
 import 'package:qassim/core/usable_functions/validate_check.dart';
 import 'package:qassim/core/utils/app_localization.dart';
-import 'package:qassim/core/utils/app_routes_utils/app_navigator.dart';
 import 'package:qassim/core/utils/design_utils/app_colors.dart';
 import 'package:qassim/core/utils/design_utils/app_images.dart';
 import 'package:qassim/core/utils/design_utils/app_sizes.dart';
 import 'package:qassim/core/utils/design_utils/app_text_styles.dart';
+import 'package:qassim/features/authentication/presentation/view_model_manger/register_cubit/register_cubit.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -16,79 +17,113 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child:  Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset(
-            AppImages.blackLightLogo,
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.58,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: AppSizes.paddingSizeDefault),
-              child: Form(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    CustomTextField(
-                      prefixIcon: Icons.person,
-                    labelText: AppLocalizations.of(context).translate('full_name'),
-                      validator: (value) =>ValidateCheck.validateEmptyText(value, context, 'full_name_required') ,
+          child: BlocConsumer<RegisterCubit, RegisterState>(
+        listener: (context, state) {
+          if (state is RegisterSuccessfulState) {
+            RegisterCubit.get(context).onRegisterSuccess(context, state.data);
+          }
+        },
+        builder: (context, state) {
+          RegisterCubit cubit = RegisterCubit.get(context);
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(
+                AppImages.blackLightLogo,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.58,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: AppSizes.paddingSizeDefault),
+                  child: Form(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CustomTextField(
+                          controller: cubit.nameController,
+                          prefixIcon: Icons.person,
+                          labelText: AppLocalizations.of(context)
+                              .translate('full_name'),
+                          validator: (value) => ValidateCheck.validateEmptyText(
+                              value, context, 'full_name_required'),
+                        ),
+                        CustomTextField(
+                          controller: cubit.emailController,
+                          prefixIcon: Icons.alternate_email,
+                          labelText:
+                              AppLocalizations.of(context).translate('email'),
+                          validator: (value) =>
+                              ValidateCheck.validateEmail(value, context),
+                        ),
+                        CustomTextField(
+                          controller: cubit.phoneController,
+                          prefixIcon: Icons.call,
+                          inputType: TextInputType.number,
+                          labelText: AppLocalizations.of(context)
+                              .translate('phone_number'),
+                          validator: (value) => ValidateCheck.validateEmptyText(
+                              value, context, 'phone_number_required'),
+                        ),
+                        CustomTextField(
+                          controller: cubit.passwordController,
+                          prefixIcon: Icons.lock,
+                          labelText: AppLocalizations.of(context)
+                              .translate('password'),
+                          isPassword: true,
+                          validator: (value) =>
+                              ValidateCheck.validatePassword(value, context),
+                        ),
+                        CustomTextField(
+                          controller: cubit.passwordConfirmationController,
+                          prefixIcon: Icons.lock,
+                          labelText: AppLocalizations.of(context)
+                              .translate('confirm_password'),
+                          isPassword: true,
+                          validator: (value) =>
+                              ValidateCheck.validateConfirmPassword(value,
+                                  context, cubit.passwordController.text),
+                        ),
+                        (state is RegisterLoadingState)
+                            ? const Center(
+                                child: CircularProgressIndicator(
+                                  color: AppColors.complementaryColor1,
+                                ),
+                              )
+                            : ElevatedButton(
+                                onPressed: () {
+                                  cubit.register(context);
+                                },
+                                child: Text(
+                                  AppLocalizations.of(context)
+                                      .translate('register'),
+                                  style: AppTextStyles.elevatedButtonTextStyle,
+                                )),
+                      ],
+                    ),
                   ),
-
-                    CustomTextField(
-                      prefixIcon: Icons.alternate_email,
-                      labelText: AppLocalizations.of(context).translate('email'),
-                      validator:(value) =>ValidateCheck.validateEmail(value, context),
-                    ),
-                    CustomTextField(
-                      prefixIcon: Icons.call,
-                      inputType: TextInputType.number,
-                      labelText: AppLocalizations.of(context).translate('phone_number'),
-                      validator: (value) =>ValidateCheck.validateEmptyText(value, context, 'phone_number_required') ,
-                    ),
-                    CustomTextField(
-                      prefixIcon: Icons.lock,
-                      labelText:
-                      AppLocalizations.of(context).translate('password'),
-                      isPassword: true,
-                      validator:(value) =>ValidateCheck.validatePassword(value, context),
-                    ),
-                    CustomTextField(
-                      prefixIcon: Icons.lock,
-                      labelText:
-                      AppLocalizations.of(context).translate('confirm_password'),
-                      isPassword: true,
-                      validator:(value) =>ValidateCheck.validateConfirmPassword(value, context,''),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {},
-                        child: Text(
-                          AppLocalizations.of(context).translate('register'),
-                          style: AppTextStyles.elevatedButtonTextStyle,
-                        )),
-                  ],
                 ),
               ),
-            ),
-          ),
-          RichText(
-            text: TextSpan(
-                text: AppLocalizations.of(context)
-                    .translate('have_account'),
-                style: AppTextStyles.textButtonTextStyle,
-                children: [
-                  TextSpan(
-                      text: AppLocalizations.of(context).translate('login'),
-                      style: AppTextStyles.textButtonTextStyle
-                          .copyWith(color: AppColors.complementaryColor2),
-                      recognizer: TapGestureRecognizer()..onTap = () {
-                        AppNavigator.navigateToLoginScreen(context);
-                      })
-                ]),
-          ),
-        ],)
-      ),
+              RichText(
+                text: TextSpan(
+                    text:
+                        AppLocalizations.of(context).translate('have_account'),
+                    style: AppTextStyles.textButtonTextStyle,
+                    children: [
+                      TextSpan(
+                          text: AppLocalizations.of(context).translate('login'),
+                          style: AppTextStyles.textButtonTextStyle
+                              .copyWith(color: AppColors.complementaryColor2),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              cubit.navigateToLoginScreen(context);
+                            })
+                    ]),
+              ),
+            ],
+          );
+        },
+      )),
     );
   }
 }
