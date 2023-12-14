d5efc329590d6a096b81add97da9a64c231a43b9
sheha <shehabehab@gmail.com>
2023-12-14 02:06:16 +0200

    add register cubit & register states

---
+++ lib/features/authentication/presentation/view_model_manger/register_cubit/register_cubit.dart
@@ -0,0 +1,73 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qassim/core/usable_functions/validate_check.dart';
+import 'package:qassim/core/utils/api_utils/api_response.dart';
+import 'package:qassim/core/utils/app_routes_utils/app_navigator.dart';
+import 'package:qassim/features/authentication/data/models/register_model.dart';
+import 'package:qassim/features/authentication/data/repositories/register/register_repo.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  RegisterCubit(this.registerRepo) : super(const RegisterInitial());
+  final RegisterRepo registerRepo;
+
+  static RegisterCubit get(BuildContext context) => BlocProvider.of(context);
+
+  //#region private variables
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _passwordConfirmationController =
+      TextEditingController();
+
+  //#endregion
+
+  //#region getters
+  TextEditingController get emailController => _emailController;
+
+  TextEditingController get nameController => _nameController;
+
+  TextEditingController get phoneController => _phoneController;
+
+  TextEditingController get passwordController => _passwordController;
+
+  TextEditingController get passwordConfirmationController =>
+      _passwordConfirmationController;
+
+  GlobalKey<FormState> get formKey => _formKey;
+
+//#endregion
+
+  //#region private methods
+  _navigateToLoginScreen(BuildContext context) {
+    AppNavigator.navigateToLoginScreen(context);
+  }
+
+  //#endregion
+
+  //#region public methods
+  Future<void> register(BuildContext context) async {
+    if (ValidateCheck.validate(_formKey)) {
+      RegisterDataModel registerDataModel = RegisterDataModel(
+          email: _emailController.text,
+          name: _nameController.text,
+          phone: _phoneController.text,
+          password: _passwordController.text,
+          passwordConfirmation: _passwordConfirmationController.text);
+      ApiResponse apiResponse =
+          await registerRepo.register(registerDataModel: registerDataModel);
+      if (apiResponse.response?.statusCode != null && apiResponse.response?.statusCode == 200) {
+        emit(const RegisterSuccessfulState());
+      }
+    }
+  }
+
+  void onRegisterSuccess(BuildContext context){
+    _navigateToLoginScreen(context);
+  }
+//#endregion
+}
---
+++ lib/features/authentication/presentation/view_model_manger/register_cubit/register_state.dart
@@ -0,0 +1,29 @@
+part of 'register_cubit.dart';
+
+abstract class RegisterState extends Equatable {
+  const RegisterState();
+}
+
+class RegisterInitial extends RegisterState {
+  const RegisterInitial();
+  @override
+  List<Object> get props => [];
+}
+
+class RegisterLoadingState extends RegisterState {
+  const RegisterLoadingState();
+  @override
+  List<Object> get props => [];
+}
+
+class RegisterSuccessfulState extends RegisterState {
+  const RegisterSuccessfulState();
+  @override
+  List<Object> get props => [];
+}
+
+class RegisterFailedState extends RegisterState {
+  const RegisterFailedState();
+  @override
+  List<Object> get props => [];
+}
