4164a6d0d8eee63b7ccbf8a8695faee1e8d73a09
sheha <shehabehab@gmail.com>
2023-12-15 02:55:06 +0200

    add login repo

--- lib/features/authentication/data/repositories/login/login_repo.dart
+++ lib/features/authentication/data/repositories/login/login_repo.dart
@@ -1,7 +1,7 @@
 import 'package:qassim/core/utils/api_utils/api_response.dart';
 import 'package:qassim/features/authentication/data/models/login_model.dart';
 
-abstract final class LoginRepo {
+abstract interface class LoginRepo {
   Future<ApiResponse> login({required String email, required String password});
   Future<({bool successful, String? errorString})> addUserDataToDB({required User user});
 }
