1886a858854a010457c43b0208f99a230e7fece7
sheha <shehabehab@gmail.com>
2023-12-15 02:46:51 +0200

    add database object to sl

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -6,20 +6,31 @@ import 'package:qassim/core/utils/api_utils/api_endpoints.dart';
 import 'package:qassim/core/utils/api_utils/logging_interceptor.dart';
 import 'package:qassim/core/utils/network_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initServicesLocator() async {
   //Core
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfo(sl<Connectivity>()));
-  sl.registerLazySingleton<DioClient>(() => DioClient(ApiEndPoints.baseUrl,sl<Dio>(),loggingInterceptor: sl<LoggingInterceptor>(), sharedPreferences: sl<SharedPreferences>()));
-
+  sl.registerLazySingleton<DioClient>(() => DioClient(
+      ApiEndPoints.baseUrl, sl<Dio>(),
+      loggingInterceptor: sl<LoggingInterceptor>(),
+      sharedPreferences: sl<SharedPreferences>()));
 
   // External
   final sharedPreferences = await SharedPreferences.getInstance();
+  final dataBase = await openDatabase(
+    'userData.db',
+    version: 1,
+    onCreate: (db, version) async {
+      await db.execute(
+          'CREATE TABLE users (userID INTEGER Primary Key,userName text, userEmail text ,  userPhone text, access_token text,);');
+    },
+  );
   sl.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
   sl.registerLazySingleton<Dio>(() => Dio());
   sl.registerLazySingleton<LoggingInterceptor>(() => LoggingInterceptor());
   sl.registerLazySingleton<Connectivity>(() => Connectivity());
-
+  sl.registerLazySingleton<Database>(() => dataBase);
 }
