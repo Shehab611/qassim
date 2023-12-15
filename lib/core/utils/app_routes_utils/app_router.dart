44111b179e526ddade0c67dc977a7363b5743bb1
sheha <shehabehab@gmail.com>
2023-12-15 04:08:00 +0200

    add login cubit provider in app routes

--- lib/core/utils/app_routes_utils/app_router.dart
+++ lib/core/utils/app_routes_utils/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qassim/core/usable_functions/api_service_helper.dart';
+import 'package:qassim/features/authentication/data/repositories/login/login_repo_impl.dart';
 import 'package:qassim/features/authentication/data/repositories/register/register_repo_impl.dart';
+import 'package:qassim/features/authentication/presentation/view_model_manger/login_cubit/login_cubit.dart';
 import 'package:qassim/features/authentication/presentation/view_model_manger/register_cubit/register_cubit.dart';
 import 'package:qassim/features/authentication/presentation/views/change_password_screen.dart';
 import 'package:qassim/features/authentication/presentation/views/forget_password_screen.dart';
@@ -16,7 +18,10 @@ import 'app_paths.dart';
 abstract final class AppRouter {
   static final Map<String, WidgetBuilder> routes = {
     AppPathName.kOpenScreen: (BuildContext context) => const OpenScreen(),
-    AppPathName.kLoginScreen: (BuildContext context) => const LoginScreen(),
+    AppPathName.kLoginScreen: (BuildContext context) => BlocProvider(
+          create: (context) => LoginCubit(LoginRepoImpl(sl<DioClient>())),
+          child: const LoginScreen(),
+        ),
     AppPathName.kRegisterScreen: (BuildContext context) => BlocProvider(
           create: (context) => RegisterCubit(RegisterRepoImpl(sl<DioClient>())),
           child: const RegisterScreen(),
