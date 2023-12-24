85c054aa9710254196e55990de3adf0430a1d538
Shehab Ehab <shehabehab1029@gmail.com>
2023-12-24 15:18:22 +0200

    add app bar
    Signed-off-by: Shehab Ehab <shehabehab1029@gmail.com>

--- lib/features/favourites/presentation/view/favourite_screen.dart
+++ lib/features/favourites/presentation/view/favourite_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:qassim/core/utils/app_constants/app_localization.dart';
+import 'package:qassim/core/utils/app_constants/app_strings.dart';
+import 'package:qassim/features/drawer/presentation/view/app_drawer.dart';
 
 class FavouriteScreen extends StatelessWidget {
   const FavouriteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: const AppDrawer(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+            AppLocalizations.of(context).translate(AppStrings.favouritePlaces)),
+      ),
+    );
   }
 }
