fe2552ac50a24d5c1d84eafe45060e503113b744
Shehab Ehab <shehabehab1029@gmail.com>
2023-12-25 20:51:54 +0200

    add place details screen file

---
+++ lib/features/home/presentation/views/place_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlaceDetailsScreen extends StatelessWidget {
+  const PlaceDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
