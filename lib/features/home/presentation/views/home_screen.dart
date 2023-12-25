82c73f66ae642e141396909e63ea72c07cb43ca1
Shehab Ehab <shehabehab1029@gmail.com>
2023-12-25 18:04:45 +0200

    start in home page design

--- lib/features/home/presentation/views/home_screen.dart
+++ lib/features/home/presentation/views/home_screen.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:qassim/core/utils/app_constants/app_localization.dart';
+import 'package:qassim/core/utils/app_constants/app_strings.dart';
 import 'package:qassim/features/drawer/presentation/view/app_drawer.dart';
+import 'package:qassim/features/home/presentation/widgets/place_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      top: true,
-      child: Scaffold(
-        body: Center(
-          child: Text('Home Screen'),
-        ),
-        drawer: AppDrawer(),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(AppLocalizations.of(context).translate(AppStrings.homePage)),
+      ),
+      drawer: const AppDrawer(),
+      body: ListView.builder(
+        itemCount: 15,
+        itemBuilder: (context, index) {
+          return const PlaceItem(imagePath: 'imagePath', title: 'title', visitDate: 'visitDate');
+        },
       ),
     );
   }
