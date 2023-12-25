565a3e2615149dc185fa915ca0cab1c1d5915a42
Shehab Ehab <shehabehab1029@gmail.com>
2023-12-25 13:07:18 +0200

    finish favourites repos

--- lib/features/favourites/data/repositories/favourites_repo_impl.dart
+++ lib/features/favourites/data/repositories/favourites_repo_impl.dart
@@ -1,4 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:qassim/core/usable_functions/api_service_helper.dart';
+import 'package:qassim/core/utils/api_utils/api_endpoints.dart';
+import 'package:qassim/core/utils/api_utils/api_error_handler.dart';
 import 'package:qassim/core/utils/api_utils/api_response.dart';
 
 import 'favourites_repo.dart';
@@ -9,20 +12,41 @@ final class FavouritesRepoImpl implements FavouritesRepo {
   FavouritesRepoImpl(this._dioClient);
 
   @override
-  Future<ApiResponse> addToFavourites(String placeId) async {
-    // TODO: implement addToFavourites
-    throw UnimplementedError();
+  Future<ApiResponse> addToFavourites(String placeId, String userId) async {
+    try {
+      StackTrace stackTrace = StackTrace.current;
+      Response responseValue = await _dioClient.post(
+          ApiEndPoints.addToFavourites, stackTrace,
+          data: {'id': userId, 'place_id': placeId});
+      return ApiResponse.withSuccess(responseValue);
+    } catch (e) {
+      return ApiResponse.withError(ApiErrorHandler.getMessage(e));
+    }
   }
 
   @override
-  Future<ApiResponse> getFavourites() async {
-    // TODO: implement getFavourites
-    throw UnimplementedError();
+  Future<ApiResponse> getFavourites(String userId) async {
+    try {
+      StackTrace stackTrace = StackTrace.current;
+      Response responseValue = await _dioClient
+          .post(ApiEndPoints.getFavourites, stackTrace, data: {'id': userId});
+      return ApiResponse.withSuccess(responseValue);
+    } catch (e) {
+      return ApiResponse.withError(ApiErrorHandler.getMessage(e));
+    }
   }
 
   @override
-  Future<ApiResponse> removeFromFavourites(String placeId) async {
-    // TODO: implement removeFromFavourites
-    throw UnimplementedError();
+  Future<ApiResponse> removeFromFavourites(
+      String placeId, String userId) async {
+    try {
+      StackTrace stackTrace = StackTrace.current;
+      Response responseValue = await _dioClient.post(
+          ApiEndPoints.removeFromFavourites, stackTrace,
+          data: {'id': userId, 'place_id': placeId});
+      return ApiResponse.withSuccess(responseValue);
+    } catch (e) {
+      return ApiResponse.withError(ApiErrorHandler.getMessage(e));
+    }
   }
 }
