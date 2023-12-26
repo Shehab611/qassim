de706df0b32e27d0320b67fa8b01c82dd95a62a7
Shehab Ehab <shehabehab1029@gmail.com>
2023-12-26 09:20:17 +0200

    edit in details screen some design

--- lib/features/home/presentation/views/place_details_screen.dart
+++ lib/features/home/presentation/views/place_details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:qassim/core/utils/app_constants/app_localization.dart';
+import 'package:qassim/core/utils/app_constants/app_strings.dart';
+import 'package:qassim/core/utils/design_utils/app_text_styles.dart';
 
 class PlaceDetailsScreen extends StatelessWidget {
   const PlaceDetailsScreen({super.key});
@@ -8,54 +11,45 @@ class PlaceDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      bottomNavigationBar: ButtonBar(
+        alignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                AppLocalizations.of(context).translate(AppStrings.addToFavourites),
+                style: AppTextStyles.elevatedButtonTextStyle,
+              )),
+          ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                AppLocalizations.of(context).translate(AppStrings.booking),
+                style: AppTextStyles.elevatedButtonTextStyle,
+              )),
+        ],
+      ),
       body: Column(
         children: [
-          Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              ClipRRect(
-                clipBehavior: Clip.hardEdge,
-                borderRadius: const BorderRadius.vertical(bottom: Radius.circular(35)),
-                child: CachedNetworkImage(
-                  imageUrl: 'https://th.bing.com/th/id/OIP.V5XDYGmtMaObFxADf1VgUAHaE6?rs=1&pid=ImgDetMain',
-                  height: 200,
-                  width: double.infinity,
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Place Title',
-                  textAlign: TextAlign.center,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Text(
-            'Place Category',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w500,
+          ClipRRect(
+            clipBehavior: Clip.hardEdge,
+            borderRadius: const BorderRadius.vertical(bottom: Radius.circular(35)),
+            child: CachedNetworkImage(
+              imageUrl: 'https://th.bing.com/th/id/OIP.V5XDYGmtMaObFxADf1VgUAHaE6?rs=1&pid=ImgDetMain',
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.fitWidth,
             ),
           ),
-          Padding(
+          RichText(
+            text: TextSpan(
+                text: '${AppLocalizations.of(context).translate(AppStrings.type)} :',
+                style: AppTextStyles.secondaryTextStyle,
+                children: [TextSpan(text: 'مكان اثري')]),
+          ),
+          const Padding(
             padding: EdgeInsets.all(8.0),
             child: Text(
-              'Place OverView',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
+                'يتميز هذا المكان بكونه مش عارف ايه و بني في عصر مش عارف ايه و يفيد انه احا الشبشب داع يتميز هذا المكان بكونه مش عارف ايه و بني في عصر مش عارف ايه و يفيد انه احا الشبشب داع يتميز هذا المكان بكونه مش عارف ايه و بني في عصر مش عارف ايه و يفيد انه احا الشبشب داع يتميز هذا المكان بكونه مش عارف ايه و بني في عصر مش عارف ايه و يفيد انه احا الشبشب داع يتميز هذا المكان بكونه مش عارف ايه و بني في عصر مش عارف ايه و يفيد انه احا الشبشب داع'),
           )
         ],
       ),
