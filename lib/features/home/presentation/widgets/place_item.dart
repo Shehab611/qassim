9c1d6c7a0f681a62716f440357a2aec4a4460787
Shehab Ehab <shehabehab1029@gmail.com>
2023-12-28 19:26:30 +0200

    edit place item to fit the new design

--- lib/features/home/presentation/widgets/place_item.dart
+++ lib/features/home/presentation/widgets/place_item.dart
@@ -1,34 +1,39 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:qassim/core/utils/design_utils/app_sizes.dart';
+import 'package:qassim/core/utils/design_utils/app_text_styles.dart';
 
 class PlaceItem extends StatelessWidget {
-  const PlaceItem(
-      {super.key, required this.imagePath, required this.title, required this.category, this.onTap});
+  const PlaceItem({super.key, required this.imagePath, required this.title, this.category, this.onTap});
 
-  final String imagePath, title, category;
+  final String imagePath, title;
+  final String? category;
   final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        onTap: onTap,
-        subtitle: Text(
-          category,
-        ),
-        contentPadding: const EdgeInsets.all(AppSizes.paddingSizeExtraSmall),
-        leading: ClipRRect(
-          borderRadius: BorderRadius.circular(12),
-          clipBehavior: Clip.antiAlias,
-          child: CachedNetworkImage(
-            imageUrl: imagePath,
-            width: 80,
-            height: null,
-            fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: 110,
+        child: Card(
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                clipBehavior: Clip.antiAlias,
+                child: CachedNetworkImage(
+                  imageUrl: imagePath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Text(
+                title,
+                style: AppTextStyles.tileTextStyle,
+                textAlign: TextAlign.center,
+              )
+            ],
           ),
         ),
-        title: Text(title),
       ),
     );
   }
