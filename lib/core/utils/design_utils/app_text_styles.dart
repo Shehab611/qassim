04d8e2ed4e04d538c0c1004d2de86fb3ace015c9
Shehab Ehab <shehabehab1029@gmail.com>
2023-12-28 21:24:43 +0200

    add new text style and refactor the file

--- lib/core/utils/design_utils/app_text_styles.dart
+++ lib/core/utils/design_utils/app_text_styles.dart
@@ -3,30 +3,38 @@ import 'package:qassim/core/utils/design_utils/app_colors.dart';
 import 'package:qassim/core/utils/design_utils/app_sizes.dart';
 
 abstract final class AppTextStyles {
+  //#region Buttons Text Styles
   static const elevatedButtonTextStyle = TextStyle(
       color: AppColors.elevatedButtonTextColor,
       fontSize: AppSizes.fontSizeExtraLarge,
       fontWeight: FontWeight.w600);
 
   static const textButtonTextStyle = TextStyle(
-      fontSize: AppSizes.fontSizeLarge,
-      fontWeight: FontWeight.w600,
-      color: AppColors.complementaryColor3);
+      fontSize: AppSizes.fontSizeLarge, fontWeight: FontWeight.w600, color: AppColors.complementaryColor3);
 
+  static const switchTextStyle = TextStyle(
+      fontSize: AppSizes.fontSizeLarge, fontWeight: FontWeight.w600, color: AppColors.complementaryColor3);
+
+  //#endregion
+
+  //#region List Tile Text Styles
   static const tileTextStyle = TextStyle(
-      fontSize: AppSizes.fontSizeLarge,
-      fontWeight: FontWeight.w600,
-      color: AppColors.complementaryColor3);
+      fontSize: AppSizes.fontSizeLarge, fontWeight: FontWeight.w600, color: AppColors.complementaryColor3);
 
   static final tileSubTitleTextStyle = TextStyle(
       fontSize: AppSizes.fontSizeDefault,
       fontWeight: FontWeight.w600,
       color: AppColors.complementaryColor3.withOpacity(.6));
 
-  static const appBarTitleTextStyle = TextStyle(
-      color: AppColors.lighterShadeColor1,
-      fontSize: AppSizes.fontSizeExtraLarge,
-      fontWeight: FontWeight.w600);
+  static const tileSecondaryTextStyle = TextStyle(
+      fontSize: AppSizes.fontSizeLarge, fontWeight: FontWeight.w600, color: AppColors.lighterShadeColor1);
+
+  static const tileSecondarySubTitleTextStyle = TextStyle(
+      fontSize: AppSizes.fontSizeDefault, fontWeight: FontWeight.w600, color: AppColors.lighterShadeColor1);
+
+  //#endregion
+
+  //#region App Default Text Styles
 
   static const defaultTextStyle = TextStyle(
       color: AppColors.lighterShadeColor1,
@@ -38,8 +46,12 @@ abstract final class AppTextStyles {
       fontSize: AppSizes.fontSizeExtraLarge,
       fontWeight: FontWeight.w600);
 
-  static const switchTextStyle = TextStyle(
-      fontSize: AppSizes.fontSizeLarge,
-      fontWeight: FontWeight.w600,
-      color: AppColors.complementaryColor3);
+  //#endregion
+
+  //#region App Bar Text Style
+  static const appBarTitleTextStyle = TextStyle(
+      color: AppColors.lighterShadeColor1,
+      fontSize: AppSizes.fontSizeExtraLarge,
+      fontWeight: FontWeight.w600);
+//#endregion
 }
