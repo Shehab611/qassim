bb918b12110e4758989c8f935c2eb2aed68b04b5
Shehab Ehab <shehabehab1029@gmail.com>
2023-12-28 20:19:19 +0200

    add search bar to home screen

--- lib/features/home/presentation/views/home_screen.dart
+++ lib/features/home/presentation/views/home_screen.dart
@@ -4,6 +4,8 @@ import 'package:qassim/core/components/custom_loader.dart';
 import 'package:qassim/core/components/no_data_screen.dart';
 import 'package:qassim/core/utils/app_constants/app_localization.dart';
 import 'package:qassim/core/utils/app_constants/app_strings.dart';
+import 'package:qassim/core/utils/design_utils/app_colors.dart';
+import 'package:qassim/core/utils/design_utils/app_text_styles.dart';
 import 'package:qassim/features/drawer/presentation/view/app_drawer.dart';
 import 'package:qassim/features/home/presentation/view_model_manger/all_places_cubit/all_places_cubit.dart';
 import 'package:qassim/features/home/presentation/widgets/place_item.dart';
@@ -25,19 +27,71 @@ class HomeScreen extends StatelessWidget {
             return const CustomLoader();
           } else if (state is AllPlacesGetDataSuccessState) {
             if (state.model.data.isNotEmpty) {
-              return ListView.builder(
-                itemCount: state.model.data.length,
-                itemBuilder: (context, index) {
-                  return PlaceItem(
-                    onTap: () {
-                      AllPlacesCubit.get(context)
-                          .navigateToPlaceDetails(context, state.model.data[index].id.toString());
-                    },
-                    imagePath: state.model.data[index].images,
-                    title: state.model.data[index].name,
-                    //category: state.model.data[index].type
-                  );
-                },
+              return CustomScrollView(
+                physics: const NeverScrollableScrollPhysics(),
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: SizedBox(
+                        height: 55,
+                        child: SearchAnchor.bar(
+                          barBackgroundColor:
+                              const MaterialStatePropertyAll<Color>(AppColors.complementaryColor3),
+                          barHintText: AppLocalizations.of(context).translate(AppStrings.search),
+                          barHintStyle:
+                              const MaterialStatePropertyAll<TextStyle>(AppTextStyles.defaultTextStyle),
+                          isFullScreen: false,
+                          barTextStyle:
+                              const MaterialStatePropertyAll<TextStyle>(AppTextStyles.defaultTextStyle),
+                          viewBackgroundColor: AppColors.complementaryColor3,
+                          barLeading: const Icon(
+                            Icons.search_sharp,
+                            color: AppColors.lighterShadeColor1,
+                          ),
+                          searchController: AllPlacesCubit.get(context).searchController,
+                          suggestionsBuilder: (BuildContext context, SearchController controller) {
+                            final keyword = controller.value.text;
+                            return List.generate(5, (index) => 'Item $index')
+                                .where((element) => element.toLowerCase().startsWith(keyword.toLowerCase()))
+                                .map((item) => GestureDetector(
+                                      onTap: () {
+                                        FocusScope.of(context).unfocus();
+                                        controller.closeView(item);
+                                      },
+                                      child: ListTile(
+                                        title: Text(item, style: const TextStyle(color: Colors.white)),
+                                        onTap: () {
+                                          controller.closeView(item);
+                                          FocusScope.of(context).unfocus();
+                                        },
+                                      ),
+                                    ));
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  SliverFillRemaining(
+                    hasScrollBody: true,
+                    child: ListView.builder(
+                      itemCount: state.model.data.length,
+                      itemBuilder: (context, index) {
+                        return PlaceItem(
+                            onTap: () {
+                              AllPlacesCubit.get(context)
+                                  .navigateToPlaceDetails(context, state.model.data[index].id.toString());
+                            },
+                            imagePath:
+                                'https://i.natgeofe.com/n/535f3cba-f8bb-4df2-b0c5-aaca16e9ff31/giza-plateau-pyramids.jpg?w=1200'
+                            // state.model.data[index].images
+                            ,
+                            title: state.model.data[index].name,
+                            category: state.model.data[index].type);
+                      },
+                    ),
+                  )
+                ],
               );
             }
           }
--- lib/features/home/presentation/widgets/place_item.dart
+++ lib/features/home/presentation/widgets/place_item.dart
@@ -1,39 +1,34 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:qassim/core/utils/design_utils/app_text_styles.dart';
+import 'package:qassim/core/utils/design_utils/app_sizes.dart';
 
 class PlaceItem extends StatelessWidget {
-  const PlaceItem({super.key, required this.imagePath, required this.title, this.category, this.onTap});
+  const PlaceItem(
+      {super.key, required this.imagePath, required this.title, required this.category, this.onTap});
 
-  final String imagePath, title;
-  final String? category;
+  final String imagePath, title, category;
   final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: SizedBox(
-        width: 110,
-        child: Card(
-          child: Column(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                clipBehavior: Clip.antiAlias,
-                child: CachedNetworkImage(
-                  imageUrl: imagePath,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Text(
-                title,
-                style: AppTextStyles.tileTextStyle,
-                textAlign: TextAlign.center,
-              )
-            ],
+    return Card(
+      child: ListTile(
+        onTap: onTap,
+        subtitle: Text(
+          category,
+        ),
+        contentPadding: const EdgeInsets.all(AppSizes.paddingSizeExtraSmall),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          clipBehavior: Clip.antiAlias,
+          child: CachedNetworkImage(
+            imageUrl: imagePath,
+            width: 80,
+            height: null,
+            fit: BoxFit.cover,
           ),
         ),
+        title: Text(title),
       ),
     );
   }
